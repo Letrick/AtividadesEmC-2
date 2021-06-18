59d356259daaaebc83b8626081bf2114d6381cb0
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-06-17 23:26:46 -0300

    Add files via upload

---
+++ exercicio5 - While.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+//Exercicio 5 - WHILE
+int main () {
+	
+	int numPar = 1;
+		while ( numPar <= 100){
+		
+			if (numPar % 2 == 0)
+			std::cout << numPar << "\n";
+			numPar++;
+		}
+		
+	}
+	
+	
+	
+	
---
+++ exercicio5-ok.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+//Exercicio 5 - FOR
+int main () {
+	
+	for (int contagem = 1; contagem <= 100; contagem++){
+	if (contagem % 2 == 1)
+		std::cout << "\n" << contagem; 
+}
+	
+	
+	
+	
+	
+	
+	
+}
