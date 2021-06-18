e5c2e56378629ff5fc456f22c4f9ad60cc7872a4
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-06-17 22:50:22 -0300

    Add files via upload

---
+++ exercicio2VersaoComDO.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+//Exercicio 2 - versao DO 
+int main () {
+	
+	int num = 100;
+		do {
+			
+			std::cout << num <<"\n";
+				num--;
+		}while( num >= 1 );
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ exercicio2VersaoFOR.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+//Exercicio 2
+int main () {
+	
+	for (int contagem = 100; contagem <= 200; contagem++){
+	
+		std::cout << "\n" << contagem; 
+}
+	
+	
+	
+	
+	
+	
+	
+}
