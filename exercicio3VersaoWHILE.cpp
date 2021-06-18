c4092137b8a028cfa969f28ec4e9c399de3e1c6a
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-06-17 23:22:51 -0300

    Add files via upload

---
+++ exercicio3VersaoWHILE.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+//Exercicio 3 - WHILE
+int main () {
+	
+	int contagem = 100;
+		
+		while ( contagem <= 200 ){
+			std::cout << contagem << "\n";
+			contagem++;
+
+			}
+	
+	
+	
+	
+	
+	
+}
