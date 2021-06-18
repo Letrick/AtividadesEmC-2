27d6170adad7a4aa10f2273744ed534f44edd99a
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-06-17 23:18:06 -0300

    Add files via upload

---
+++ exercicio2VersaoComFOR.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+//Exercicio 2 - versao FOR
+int main () {
+	
+	for(int contagem = 100;  contagem >= 1; contagem--){
+		std::cout << contagem << "\n";
+		
+	}
+	
+	
+	
+	
+	
+	
+}
