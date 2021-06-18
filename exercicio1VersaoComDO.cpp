a1693c57dfb5f23d43129ceb34dd6dbc5983af05
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-06-17 22:40:03 -0300

    Add files via upload

---
+++ exercicio1VersaoComDO.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+int main () {
+	int num = 0;
+	do{
+		std::cout << "\n" << num;
+		num++;
+	}
+		while (num <=50);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ exercicio1VersaoComFOR.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main () {
+	
+	for (int contagem = 50; contagem >= 1; contagem--){
+	
+		std::cout << "\n" << contagem; 
+}
+	
+	
+	
+	
+	
+	
+	
+}
