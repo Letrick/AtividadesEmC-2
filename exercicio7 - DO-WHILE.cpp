38a991306e1fffe657f777212ea36991ae379689
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-06-17 23:31:41 -0300

    Add files via upload

---
+++ exercicio7 - DO-WHILE.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+  //Exercicio 7 - DO - WHILE
+
+int main(){
+	
+		int num = 1;
+		
+		do{
+			
+				std::cout << num * num  << "\n";
+				num++;
+		}while(num <= 20);
+}
---
+++ exercicio7 - FOR.cpp
@@ -0,0 +1,20 @@
+ #include <iostream>
+ //Exercicio 7 - FOR
+int main(){
+	
+	int num = 1;
+	 	for( num;  num <= 20; num++){
+	 		
+		std::cout << num * num << "\n";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
